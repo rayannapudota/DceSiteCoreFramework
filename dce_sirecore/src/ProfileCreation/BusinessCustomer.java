565b520eb9b00bceb342495df5de50817d3c0dac
rayannapudota <32493738+rayannapudota@users.noreply.github.com>
2020-12-09 15:05:02 -0500

    Initial Git to Git Hub Loading/commit

---
+++ dce_sirecore/src/ProfileCreation/BusinessCustomer.java
@@ -0,0 +1,16 @@
+package ProfileCreation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BusinessCustomer {
+
+	public static void main(String[] args) {
+		System.out.println("My First Java Class\n");
+		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\chromedriver_win32\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			driver.get("https://www.consumersenergy.com/");
+			
+			driver.close();
+	}
+}
